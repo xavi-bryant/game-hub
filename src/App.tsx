60706834cd26dbd4b012067f46d762865249e550
fang xinsheng <420948614@qq.com>
2023-06-24 18:26:10 +0800

    Build the responsive layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 function App() {
-
   return (
     <>
-     <Button colorScheme='blue'>Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" " main"`,
+          lg: `"nav nav" "aside main"`, //992px
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="dodgerblue">
+          main
+        </GridItem>
+      </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
