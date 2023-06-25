5a81af4b0b60b4af7f00e61567cb4d6f63597a28
fang xinsheng <420948614@qq.com>
2023-06-25 09:53:27 +0800

    Buildig game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,6 +1,7 @@
 
-import { Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import  useGames  from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 
 const GameGrid = () => {
@@ -11,11 +12,11 @@ const GameGrid = () => {
       {error ? (
         <Text>{error}</Text>
       ) : (
-        <ul>
+        <SimpleGrid columns={{sm:1,md:2,lg:3,xl:5}} padding="10px" spacing={10}>
           {games.map((game) => (
-            <li key={game.id}>{game.name}</li>
+            <GameCard key={game.id} game={game}/>
           ))}
-        </ul>
+        </SimpleGrid>
       )}
     </>
   );
